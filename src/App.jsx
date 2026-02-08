2a2877ff0d154c105786958f94a5fca5e3f28bcb
eminpolat749 <eminpolat749@gmail.com>
2026-02-08 17:46:44 +0300

    redux toolkit basic example

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/counterSlice';
+
+function App() {
+  const sayi = useSelector((state) => state.counter.value)
+
+  // 2. EMİR VERME YETKİSİ (useDispatch)
+  const dispatch = useDispatch()
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      {/* State'ten gelen veriyi ekrana basıyoruz */}
+      <h1>Şu anki Sayı: {sayi}</h1>
+
+      <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+        
+        {/* Butona basınca dispatch ile "increment" emrini yolluyoruz */}
+        <button onClick={() => dispatch(increment())}>
+          Artır (+)
+        </button>
+
+        {/* Butona basınca dispatch ile "decrement" emrini yolluyoruz */}
+        <button onClick={() => dispatch(decrement())}>
+          Azalt (-)
+        </button>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
